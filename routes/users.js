27f7708042cdeff029f398fe7972a046ad3a5ea7
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-05-27 21:37:57 +0200

    simplify  the user router logic using lodash.

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
-const { User , validate} = require("../models/User.model");
+const _ = require("lodash");
+const { User, validate } = require("../models/User.model");
 
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).json(error.details[0].message);
 
-router.post('/' , async (req , res) => {
-        const {error} = validate(req.body);
-        if(error) return res.status(400).json(error.details[0].message);
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).json("user already registered..");
 
-        let user = await User.findOne({email: req.body.email});
-        if(user) return res.status(400).json("user already registered..");
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  await user.save();
 
-        user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        });
-        await user.save();
-
-        res.json(user);
+  res.json(_.pick(user, ["name", "email", "_id"]));
 });
-
 
 module.exports = router;
