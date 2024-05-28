1a1e4b6dce519d73bb087f9c5d9cdfd8036347b8
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-05-28 21:35:31 +0200

    implement the admin middleware function and test it .

--- models/User.model.js
+++ models/User.model.js
@@ -23,10 +23,11 @@ const userSchema = new mongoose.Schema({
     minlength: 5,
     maxlength: 1024,
   },
+  isAdmin: Boolean
 });
 
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id }, "jwtPrivateKey");
+  const token = jwt.sign({ _id: this._id , isAdmin: this.isAdmin }, "jwtPrivateKey");
   return token;
 };
 
