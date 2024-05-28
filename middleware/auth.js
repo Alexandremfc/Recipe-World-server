5e76c86383e042d5e4045242b3d496f4c49c8d99
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-05-28 20:36:50 +0200

    implement the authorization middleware function.

---
+++ middleware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+function auth(req, res, next) {
+  const token = req.get("x-auth-token");
+  if (!token) return res.status(401).send("Acces denied. No token Provided.");
+  try {
+    const decoded = jwt.verify(token, "jwtPrivateKey");
+    req.user = decoded;
+    next();
+  } catch (ex) {
+    res.status(400).send("Invalid token.");
+  }
+}
+
+module.exports = auth;
