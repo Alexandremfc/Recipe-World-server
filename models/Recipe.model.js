881e5b6c9be46229bdba61f04d0d1058ca943284
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-05-30 00:55:30 +0200

    implement the Recipe Schema.

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,23 +1,48 @@
 const mongoose = require("mongoose");
 
 const recipeSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  ingridients: [String],
-  Instructions: [String],
-  Image: { 
+  title: {
     type: String,
-    required: true ,
+    required: true,
   },
-  Category:  {
+  description: {
     type: String,
-    enum : ['Breakfast','Lunch', 'Dinner', 'Snack'],
-    required: true
-  }, 
+    minlength: 20,
+    required: true,
+  },
+  ingridients: {
+    type: Array,
+    validate: {
+      validator: function (v) {
+        return v.length > 0;
+      },
+      message: "the Meal shoulld have at least one ingridient.",
+    },
+    required: true,
+  },
+  instructions: {
+    type: Array,
+    validate: {
+      validator: function (v) {
+        return v.length > 2;
+      },
+      message: "the Meal shoulld have at least three instructions.",
+    },
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    enum: ["Breakfast", "Lunch", "Dinner", "Snack"],
+    required: true,
+  },
   author: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+    ref: "User",
+    required: true,
   },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
@@ -26,7 +51,3 @@ const recipeSchema = new mongoose.Schema({
 const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;
-
-// required
-// minlength 
-// maxlength
