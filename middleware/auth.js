fbbeb4602f59a314c188ce8f8b8ed8b920b9955c
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-06-03 18:25:43 +0200

    hidding the jwt secret key.

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ function auth(req, res, next) {
   const token = req.get("x-auth-token");
   if (!token) return res.status(401).send("Acces denied. No token Provided.");
   try {
-    const decoded = jwt.verify(token, "jwtPrivateKey");
+    const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
     req.user = decoded;
     next();
   } catch (ex) {
