0582256ddf99472268943138c0a07583434eb858
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-06-03 18:38:16 +0200

    store the client ip address in env varaible.

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,7 +1,6 @@
 const cors = require("cors");
-
-const CLIENT_IP_ADDRESS = "http://localhost:5173";
+require("dotenv").config();
 
 module.exports = cors({
-  origin: [CLIENT_IP_ADDRESS],
+  origin: [process.env.CLIENT_IP_ADDRESS],
 });
