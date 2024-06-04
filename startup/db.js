2ce5707098d54090b1e53b2993322d9775058a39
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-06-04 12:30:16 +0200

    fix: env variable for db url

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
+const MONGODB_URL =
+  process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/Recipe-Book";
+
 module.exports = function () {
   mongoose
-    .connect("mongodb://localhost/Recipe-Book")
+    .connect(MONGODB_URL)
     .then(() => console.log("Connected to MongoDB.."))
     .catch((err) => console.error("Could not connect tp MongoDB", err));
 };
