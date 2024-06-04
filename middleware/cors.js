a1464d45f839a0d6486e09107076ca99dd7d181e
Alexandre Cabral <alex.miguel89@gmail.com>
2024-06-04 14:18:46 +0100

    setting up the env varraibles.

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,6 +1,8 @@
 const cors = require("cors");
 require("dotenv").config();
 
+const CLIENT_IP_ADDRESS =  process.env.CLIENT_IP_ADDRESS || "http://localhost:5173";
+
 module.exports = cors({
-  origin: [process.env.CLIENT_IP_ADDRESS],
+  origin: [CLIENT_IP_ADDRESS],
 });
